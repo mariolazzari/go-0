ea0b12931340d67aeeecee6485d97f51ef053a33
Mario Lazzari <mario.lazzari@gmail.com>
2026-05-02 23:08:13 +0200

    Done lesson 25

--- 04-point/25-metodi/main.go
+++ 04-point/25-metodi/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Cerchio struct {
+	raggio float64
+}
+
+func (c Cerchio) Area() float64 {
+	return math.Pow(c.raggio, 2) * math.Pi
+}
+
+func (c Cerchio) Circonferenza() float64 {
+	return c.raggio * 2 * math.Pi
+}
+
+func main() {
+	c := Cerchio{raggio: 10}
+	fmt.Printf("Area  cerchio: %.2f\n", c.Area())
+	fmt.Printf("Circonferenza: %.2f\n", c.Circonferenza())
+}
