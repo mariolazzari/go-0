020d12087d47f09629740e96f6e41bde69a88298
Mario Lazzari <mario.lazzari@gmail.com>
2026-05-05 09:11:58 +0200

    Started ex6

--- 07-concorrenza/ex6/main.go
+++ 07-concorrenza/ex6/main.go
@@ -1,5 +1,28 @@
 package main
 
+// Goroutine del barista
+func barista(id int, ordini chan<- int, pronti <-chan int, errors chan int) {
+
+}
+
+// Simula un tempo casuale per la preparazione del caffè
+
+// Simula un errore casuale
+
+// Invia l'ordine pronto al canale
+
+// Goroutine del cliente
+func cliente() {
+
+}
+
 func main() {
+	nBaristi := 5
+
+	// Avvia i barista
+
+	// Invia gli ordini al canale
+
+	// Gestisce gli ordini pronti e gli errori
 
 }
